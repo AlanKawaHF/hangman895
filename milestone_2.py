9e640e6aecf8440e1196288c05e0de4427cb693e
AlanKawaHF <70645103+AlanKawaHF@users.noreply.github.com>
2024-06-11 17:13:36 +0100

    Update milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -5,10 +5,23 @@ print(word_list)
 word = random.choice(word_list)
 print(word)
 
-guess = input("Please enter a single letter: ")
-print(f"You have entered: {guess}")
+def check_guess(guess):
+    guess = guess.lower()
+    if guess in word:
+        print(f"Good guess! {guess} is in the word.")
+    else:
+        print(f"Sorry, {guess} is not in the word. Try again.")
 
-if len(guess) == 1 and guess.isalpha():
-    print("Good guess!")
-else:
-    "Oops! That is not a valid input."
+def ask_for_input():
+    while True:
+
+        guess = input("Please enter a single letter: ")
+
+        if len(guess) == 1 and guess.isalpha():
+         print("Valid input")
+        
+         break
+        else:
+            "Invalid letter. Please, enter a single alphabetical character."
+
+ask_for_input()
